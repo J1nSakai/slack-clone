168d2d423c074ff72ca769e8512fba4b1ede8cbc
Taimoor <taimoor2003abrejo@gmail.com>
2025-08-24 20:27:43 +0500

    auth implemented

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true } // automatically gives us createdAt and updatedAt fields
+);
+
+export const User = mongoose.model("User", userSchema);
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,16 +1,36 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
 
 const app = express();
+
+app.use(clerkMiddleware());
+
+app.use(express.json());
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.listen(ENV.PORT, () => {
-  console.log("Server is running on port ", ENV.PORT);
-  connectDB();
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log(`Server started on port ${ENV.PORT}`);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting the server", error);
+    process.exit(1);
+  }
+};
 
-// XzISM7FisU0rOwcI
+startServer();
+
+export default app;
